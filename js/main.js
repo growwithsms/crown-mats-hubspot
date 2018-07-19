01d6e8fb932d0ba315fe9f95721f60c27de1a15b
Stefen Phelps <the@stefenphelps.com>
2018-07-19 14:21:39 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -4,59 +4,63 @@
 
 //@prepros-prepend vendor/flickity.pkgd.min.js
 
-
 /////////////////////////
 // General
 /////////////////////////
 
 // remove no-js from html tag
-document.documentElement.className = document.documentElement.className.replace("no-js", "js");
+document.documentElement.className = document.documentElement.className.replace(
+    "no-js",
+    "js"
+);
 
 // mobile menu
-var mobileNavTrigger = document.getElementById('menu-toggle');
+var mobileNavTrigger = document.getElementById("menu-toggle");
 
 // Click the mobileNavTrigger.
 mobileNavTrigger.onclick = function() {
-
     // Toggle class "opened". Set also aria-expanded to true or false.
-    if (-1 !== mobileNavTrigger.className.indexOf('opened')) {
-        mobileNavTrigger.className = mobileNavTrigger.className.replace(' opened', '');
-        mobileNavTrigger.setAttribute('aria-expanded', 'false');
+    if (-1 !== mobileNavTrigger.className.indexOf("opened")) {
+        mobileNavTrigger.className = mobileNavTrigger.className.replace(
+            " opened",
+            ""
+        );
+        mobileNavTrigger.setAttribute("aria-expanded", "false");
     } else {
-        mobileNavTrigger.className += ' opened';
-        mobileNavTrigger.setAttribute('aria-expanded', 'true');
+        mobileNavTrigger.className += " opened";
+        mobileNavTrigger.setAttribute("aria-expanded", "true");
     }
-
 };
-
 
 /////////////////////////
 // Home Page
 /////////////////////////
-$('.customer-testimonials').flickity({
+$(".customer-testimonials").flickity({
     // options
-    cellAlign: 'center',
+    cellAlign: "center",
     contain: true,
-    cellSelector: '.hs_cos_wrapper_type_custom_widget',
+    cellSelector: ".hs_cos_wrapper_type_custom_widget",
     imagesLoaded: true
 });
 
 /////////////////////////
-// Contact Page 
+// Contact Page
 /////////////////////////
-$('.locate-representative-states select').on('change', function(){
-    
+$(".locate-representative-states select").on("change", function() {
     var $currentState = "#hs_cos_wrapper_" + $(this).val();
-
-    $('.locate-representative-content > span.active').removeClass('active').addClass('deactivated');
-
-    $($currentState).removeClass('deactivated').addClass('active');
-
+    $(".locate-representative-content > div.active")
+        .removeClass("active")
+        .addClass("deactivated");
+    $($currentState)
+        .removeClass("deactivated")
+        .addClass("active");
 });
 
 /////////////////////////
-// Product Listing Page 
+// Product Listing Page
 /////////////////////////
-$('.sidebar-widget-button').on('click', function(){
-    $(this).parent().toggleClass('collapsed');
+$(".sidebar-widget-button").on("click", function() {
+    $(this)
+        .parent()
+        .toggleClass("collapsed");
 });
