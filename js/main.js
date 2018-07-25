7aed19d90a46a36420b930a0deaae4a27f056813
Stefen Phelps <the@stefenphelps.com>
2018-07-25 16:31:42 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -4,92 +4,112 @@
 
 //@prepros-prepend vendor/flickity.pkgd.min.js
 
-/////////////////////////
-// General
-/////////////////////////
+$(document).ready(function() {
+    /////////////////////////
+    // General
+    /////////////////////////
 
-// mobile menu
-var mobileNavTrigger = document.getElementById("menu-toggle");
+    // mobile menu
+    var mobileNavTrigger = document.getElementById("menu-toggle");
 
-// Click the mobileNavTrigger.
-mobileNavTrigger.onclick = function() {
-    // Toggle class "opened". Set also aria-expanded to true or false.
-    if (-1 !== mobileNavTrigger.className.indexOf("opened")) {
-        mobileNavTrigger.className = mobileNavTrigger.className.replace(
-            " opened",
-            ""
-        );
-        mobileNavTrigger.setAttribute("aria-expanded", "false");
-    } else {
-        mobileNavTrigger.className += " opened";
-        mobileNavTrigger.setAttribute("aria-expanded", "true");
-    }
-};
+    // Click the mobileNavTrigger.
+    mobileNavTrigger.onclick = function() {
+        // Toggle class "opened". Set also aria-expanded to true or false.
+        if (-1 !== mobileNavTrigger.className.indexOf("opened")) {
+            mobileNavTrigger.className = mobileNavTrigger.className.replace(
+                " opened",
+                ""
+            );
+            mobileNavTrigger.setAttribute("aria-expanded", "false");
+        } else {
+            mobileNavTrigger.className += " opened";
+            mobileNavTrigger.setAttribute("aria-expanded", "true");
+        }
+    };
 
-/////////////////////////
-// Home Page
-/////////////////////////
-$(".customer-testimonials").flickity({
-    // options
-    cellAlign: "center",
-    contain: true,
-    cellSelector: ".hs_cos_wrapper_type_custom_widget",
-    imagesLoaded: true
-});
+    /////////////////////////
+    // Home Page
+    /////////////////////////
+    $(".hero-slider-1").flickity({
+        cellSelector: ".hero-section",
+        autoPlay: true,
+        prevNextButtons: false,
+        pageDots: false
+    });
 
-/////////////////////////
-// Contact Page
-/////////////////////////
-$(".locate-representative-states select").on("change", function() {
-    var $currentState = "#hs_cos_wrapper_" + $(this).val();
-    $(".locate-representative-content > div.active")
-        .removeClass("active")
-        .addClass("deactivated");
-    $($currentState)
-        .removeClass("deactivated")
-        .addClass("active");
-});
+    $(".customer-testimonials").flickity({
+        cellAlign: "center",
+        contain: true,
+        cellSelector: ".hs_cos_wrapper_type_custom_widget",
+        imagesLoaded: true
+    });
 
-/////////////////////////
-// Product Listing Page
-/////////////////////////
-$(".sidebar-widget-button").on("click", function() {
-    $(this)
-        .parent()
-        .toggleClass("collapsed");
-});
+    $(".customer-logos .hs_cos_wrapper_type_widget_container").flickity({
+        cellAlign: "center",
+        contain: true,
+        imagesLoaded: true,
+        pageDots: false,
+        prevNextButtons: false
+    });
 
-/////////////////////////
-// Product Page
-/////////////////////////
+    /////////////////////////
+    // Contact Page
+    /////////////////////////
+    $(".locate-representative-states select").on("change", function() {
+        var $currentState = "#hs_cos_wrapper_" + $(this).val();
+        $(".locate-representative-content > div.active")
+            .removeClass("active")
+            .addClass("deactivated");
+        $($currentState)
+            .removeClass("deactivated")
+            .addClass("active");
+    });
 
-// product thumbnails
-$('.more-images a').on('click', function(e){
-    e.preventDefault();
-    var image_src = $(this).attr('href');
-    $("#main-image").attr('src', image_src);
-    $("#main-image").parent().attr('href', image_src);
-});
+    /////////////////////////
+    // Product Listing Page
+    /////////////////////////
+    $(".sidebar-widget-button").on("click", function() {
+        $(this)
+            .parent()
+            .toggleClass("collapsed");
+    });
 
-// request a sample modal
-$('.modal-trigger').on('click', function(e){
-    e.preventDefault();
-    var modal_id = $(this).attr('href');
-    $(modal_id).show();
-});
-$('.close').on('click', function(){
-    $(this).parent().parent().hide();
-});
+    /////////////////////////
+    // Product Page
+    /////////////////////////
 
-// specification tabs
-$('.tab a').on('click', function(e) {
-    e.preventDefault();
+    // product thumbnails
+    $(".more-images a").on("click", function(e) {
+        e.preventDefault();
+        var image_src = $(this).attr("href");
+        $("#main-image").attr("src", image_src);
+        $("#main-image")
+            .parent()
+            .attr("href", image_src);
+    });
 
-    $('.tab a').removeClass('active');
-    $(this).addClass('active');
+    // request a sample modal
+    $(".modal-trigger").on("click", function(e) {
+        e.preventDefault();
+        var modal_id = $(this).attr("href");
+        $(modal_id).show();
+    });
+    $(".close").on("click", function() {
+        $(this)
+            .parent()
+            .parent()
+            .hide();
+    });
 
-    $('.tabcontent').removeClass('visible');
-    var tab_id = $(this).attr('href');
-    $(tab_id).addClass('visible');
+    // specification tabs
+    $(".tab a").on("click", function(e) {
+        e.preventDefault();
 
+        $(".tab a").removeClass("active");
+        $(this).addClass("active");
+
+        $(".tabcontent").removeClass("visible");
+        var tab_id = $(this).attr("href");
+        $(tab_id).addClass("visible");
+    });
 });
